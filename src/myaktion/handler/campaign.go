fdcf4a96fdc96caeeb7f6878d7dcc5c26826779b
nidigeser <niklas.digeser@online.de>
2023-04-13 09:22:30 +0200

    added handler utils

--- src/myaktion/handler/campaign.go
+++ src/myaktion/handler/campaign.go
@@ -2,30 +2,36 @@ package handler
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/nidigeser/go-myaktion/src/myaktion/model"
 	"github.com/nidigeser/go-myaktion/src/myaktion/service"
+	log "github.com/sirupsen/logrus"
 )
 
-func CreateCampaign(w http.ResponseWriter, r *http.Request) {
+func getCampaign(r *http.Request) (*model.Campaign, error) {
 	var campaign model.Campaign
 	err := json.NewDecoder(r.Body).Decode(&campaign)
 	if err != nil {
-		log.Printf("Cannot decode request body to campaign struct: %v", err)
+		log.Errorf("Can't decode request body to campaign struct: %v", err)
+		return nil, err
+	}
+	return &campaign, nil
+}
+
+func CreateCampaign(w http.ResponseWriter, r *http.Request) {
+	var campaign *model.Campaign
+	campaign, err := getCampaign(r)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if err := service.CreateCampaign(&campaign); err != nil {
+	if err := service.CreateCampaign(campaign); err != nil {
 		log.Printf("Error calling service CreateCampaign: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(campaign); err != nil {
-		log.Printf("Failure encoding campaign to JSON: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	sendJson(w, campaign)
 }
 
 func GetCampaigns(w http.ResponseWriter, r *http.Request) {
@@ -33,10 +39,7 @@ func GetCampaigns(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error calling service GetCampaigns: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(campaigns); err != nil {
-		log.Printf("Failure encoding campaigns to JSON: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	sendJson(w, campaigns)
 }
